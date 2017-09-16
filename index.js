6c3c144cd5fb7ac70111eea2491035a6a779a702
lucas botteri <lucasbotteri@hotmail.com>
2017-09-16 16:44:18 -0300

    Agregado puerto

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port', app.get('port'));
 });
