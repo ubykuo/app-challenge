8ef20b6f11b007e1fd2a78de0ab8565a1b92f747
Santiago Figueiras <santi.figueiras95@gmail.com>
2017-09-23 04:46:41 -0300

    Trying with https for socket.io

--- front/src/main.js
+++ front/src/main.js
@@ -9,7 +9,7 @@ import VueLocalStorage from 'vue-localstorage'
 
 Vue.use(VueResource)
 Vue.use(VueLocalStorage)
-Vue.use(VueWebsocket, 'wss://app-challenge.herokuapp.com')
+Vue.use(VueWebsocket, 'https://app-challenge.herokuapp.com')
 
 Vue.config.productionTip = false
 
