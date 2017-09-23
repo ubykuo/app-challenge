fc680570aac7af9e06b7c917f1565c07aba3dd52
Valentin Mari <valen.mari@ubykuo.com>
2017-09-23 04:16:53 -0300

    Recover frontend

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 "use strict";
+const express = require('express');
 const socketSync = require(__dirname + '/socketSync');
-const server = require('http').createServer();
 const mongoose = require(__dirname + '/dbConnection');
 const config = require(__dirname + '/config');
 
+const app = express();
+
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 //Init socket
 socketSync.init(io);
 
+//Frontend routes
+app.use('/', express.static(__dirname + '/front/dist'));
+
+//Index fallback
+app.use((req, res) => {
+  res.sendFile(__dirname + '/front/dist/index.html');
+});
+
 //Start Server
-server.listen(config.port);
+server.listen(config.port, function () {
+  console.log('App Challenge listening on port', config.port);
+});
